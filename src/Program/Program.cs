0d4a8bbec12e5589233ebb2b390ccf7a523941d4
Juan Ignacio Weyrauch <juan.weyrauch@correo.ucu.edu.uy>
2024-08-12 19:20:09 -0300

    Arreglos de Manuel

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,10 +3,10 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-public class Substraction
+public class Division
 {
-    public static int Substract(int a, int b)
+    public static double Divide(int a, int b)
     {
-        return a - b;
+        return (double)a / b;
     }
 }
