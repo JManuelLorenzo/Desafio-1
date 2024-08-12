2164f41ff50c8e9201989139276669fb2de99797
Santiago Pellejero <santiago.pellejero@correo.ucu.edu.uy>
2024-08-12 19:12:12 -0300

    Multiplicar

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,3 +11,11 @@ public class Multiplication
         return a * b;
     }
 }
+
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
